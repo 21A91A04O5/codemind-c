ba9d593665e21c7dd914af1d2382ce54819dc45b
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2022-02-18 21:57:35 +0530

    //ignoring the number, the sum of factors is equal to the number #include<stdio.h> int main() {     int i,n,sum=0;     scanf("%d",&n);     i=1;     while(i<n)     {         if(n%i==0)         sum=sum+i;         i++;     }     if(sum==n)         printf("True");     else         printf("False");     return 0; }

---
+++ Perfect_Number.c
@@ -0,0 +1,19 @@
+//ignoring the number, the sum of factors is equal to the number
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0;
+    scanf("%d",&n);
+    i=1;
+    while(i<n)
+    {
+        if(n%i==0)
+        sum=sum+i;
+        i++;
+    }
+    if(sum==n)
+        printf("True");
+    else
+        printf("False");
+    return 0;
+}
