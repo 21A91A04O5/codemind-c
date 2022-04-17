522969076fcf9e7ab30449e23d54025810641062
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2022-04-17 12:55:09 +0530

    #include<stdio.h> int main() {     int x[1000],n,i,j,flag=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         flag=0;         for(j=0;j<n;j++)         {             if(i!=j)             {                  if(x[i]==x[j])                 {                     flag=1;                     break;                 }             }         }         if(flag==0)             printf("%d",x[i]);     }     return 0; }

---
+++ Single_Number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int x[1000],n,i,j,flag=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        flag=0;
+        for(j=0;j<n;j++)
+        {
+            if(i!=j)
+            {
+                 if(x[i]==x[j])
+                {
+                    flag=1;
+                    break;
+                }
+            }
+        }
+        if(flag==0)
+            printf("%d",x[i]);
+    }
+    return 0;
+}
