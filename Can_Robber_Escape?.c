1fd2c6e040a080a0ca090a07170700138052c533
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2023-01-12 22:51:08 +0530

    #include<stdio.h> int main() {     int n,c=0,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)       scanf("%d",&a[i]);     for(i=0;i<n;i++)     {         if(a[i]>=n)           c++;     }     if(c==0)        printf("YES");     else        printf("NO");     return 0; }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,26 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],i,c=0;
+    int n,c=0,i;
     scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+      scanf("%d",&a[i]);
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(n<=a[i])
-        {
-            c=1;
-            break;
-        }
+        if(a[i]>=n)
+          c++;
     }
     if(c==0)
-    {
-        printf("YES");
-    }
+       printf("YES");
     else
-    {
-        printf("NO");
-    }
+       printf("NO");
+    return 0;   
 }
